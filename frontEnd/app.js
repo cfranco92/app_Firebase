139271a39b83836ab1704e2766aa411a776b420a
Cristian Franco <cfrancobedoya@gmail.com>
2018-04-23 14:33:41 -0500

    AGREGAMOS USUARIO A LA BASE DE DATOS
    Mediante .set--> modifica algo en específico y
    .on-> utiliza para snap con "child_added" actua
    en el momento que se crea una nueva rama.

--- frontEnd/app.js
+++ frontEnd/app.js
@@ -8,8 +8,40 @@ $('#login').click(function(){
             .signInWithPopup(provider)
             .then(function(result) {
                   console.log(result.user);
+                  guardaDatos(result.user);
                   $('#login').hide(); // ESCONDEMOS HIDE
                   $('#root').append("<img  src='" + result.user.photoURL + "' />"); // AGREGAMOS HTML A UNA ETIQUETA
             });
 
 });
+
+// GUARDAR USUARIO AUTOMATICAMENTE
+function guardaDatos(user){
+  var usuario = {
+    uid: user.uid,
+    nombre: user.displayName,
+    email: user.email,
+    foto: user.photoURL
+  }
+  firebase.database().ref("usuarios/" + user.uid)
+    .set(usuario)
+    // .push(usuario);
+}
+
+// ESCRIBIR EN LA BASE DE DATOS
+$('#guardar').click(function(){
+  firebase.database().ref("telmex")  //RAMA
+    .set({ //Escribo en la base de datos
+        nombre:"Bliss",
+        edad:"15",
+        sexo:"Masculino"
+    })
+});
+
+
+// LEEMOS DE LA BASE DE DATOS
+firebase.database().ref("usuarios")
+  .on("child_added", function(s){ // SI SE AGREGA UNA NUEVA RAMA A USUARIOS
+    var user = s.val();
+    $('#root').append("<img  src='" + user.foto + "' />"); // AGREGAMOS HTML A UNA ETIQUETA    
+  })
